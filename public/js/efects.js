bc7c9cb5175db135afec0c6918a31afbcb7982f8
Joan Carlos Rosario <joancarlosrosarios@hotmail.es>
2016-09-23 18:53:22 +0000

    23-9-2016

--- public/js/efects.js
+++ public/js/efects.js
@@ -1,26 +1,38 @@
 $( function() {
     // run the currently selected effect
     function hideALL() {
-    
+   
      
-      $("#effect").hide();
-      $("#gitHubDiv").hide();
+        $("#effect").hide();
+        $("#gitHubDiv").hide();
+        $("#divResume").hide();
       
-      
-    };
+    }
+    
+    
     $( ".toggler" ).show('slide');
-    // Set effect from select menu value
     $("#gitHubDiv").hide();
+    $("#divResume").hide();
+    
+    
     
     
     $( "#linkPorfolio" ).on( "click", function() {
-      hideALL();
-       $( "#effect" ).show('slide',{}, 500);
+         hideALL();
+        $( "#effect" ).show('slide',{}, 500);
+      
     });
  
     $("#linkGithub").on('click',function(){
-      hideALL();
-      $("#gitHubDiv").show('slide',{}, 500);
+        hideALL();
+        $("#gitHubDiv").show('slide',{}, 500);
+        
     });
+    $("#linkResume").on('click',function(){
+         hideALL();
+        $("#divResume").show('slide',{}, 500);
+       
+    });
+    
     
   } );
